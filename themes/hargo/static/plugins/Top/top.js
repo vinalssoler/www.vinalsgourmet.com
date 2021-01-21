5f50c3dbd59dc6ae477fbb212448a4813f0f41eb
Jonatan <jsala@vinalssoler.com>
2021-01-21 19:43:41 +0100

    ed

---
+++ themes/hargo/static/plugins/Top/top.js
@@ -0,0 +1,14 @@
+$(function() {
+  
+    var $btn = $('#btnTop');
+    var $home = $('#topSection');
+    var startpoint = $home.scrollTop() + $home.height();
+    
+    $(window).on('scroll', function() {
+      if($(window).scrollTop() > startpoint) {
+        $btn.show();
+      } else {
+        $btn.hide();
+      }
+    });
+  });
